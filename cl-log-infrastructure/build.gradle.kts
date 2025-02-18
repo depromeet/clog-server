537689cafee5053c4ae996d2c7421993655d790a
Kijun Kwon <39583312+kkjsw17@users.noreply.github.com>
2025-02-18 14:02:59 +0900

    feat: p6spy 세팅 및 관련 의존성 추가 (#19)

--- cl-log-infrastructure/build.gradle.kts
+++ cl-log-infrastructure/build.gradle.kts
@@ -3,6 +3,8 @@ dependencies {
 
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("com.github.gavlyukovskiy:p6spy-spring-boot-starter:1.10.0")
+
     runtimeOnly("com.mysql:mysql-connector-j")
 }
 
