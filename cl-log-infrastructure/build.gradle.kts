d1254282e07d2069d8452243499353957200ba5d
Kijun Kwon <39583312+kkjsw17@users.noreply.github.com>
2025-02-21 14:10:18 +0900

    feat: Kotlin JDSL 관련 의존성 세팅 (#21)

--- cl-log-infrastructure/build.gradle.kts
+++ cl-log-infrastructure/build.gradle.kts
@@ -5,6 +5,9 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("com.github.gavlyukovskiy:p6spy-spring-boot-starter:1.10.0")
 
+    implementation("com.linecorp.kotlin-jdsl:jpql-dsl:3.5.4")
+    implementation("com.linecorp.kotlin-jdsl:jpql-render:3.5.4")
+
     runtimeOnly("com.mysql:mysql-connector-j")
 }
 
