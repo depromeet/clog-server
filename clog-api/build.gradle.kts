b34889cc6e8d42fd7fb90102bca375a4e7141827
hyemi <hyemi.choi@tsw.im>
2025-02-26 10:27:19 +0900

    feat : api 모듈에 infrastructure모듈 의존성 추가

--- clog-api/build.gradle.kts
+++ clog-api/build.gradle.kts
@@ -1,6 +1,7 @@
 dependencies {
     implementation(project(":clog-global-utils"))
     implementation(project(":clog-domain"))
+    implementation(project(":clog-infrastructure"))
 
     implementation("org.springframework.boot:spring-boot-starter-web")
 }
