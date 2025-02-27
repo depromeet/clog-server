6c9ad1e5226fca757fa5c1042081fa6434da0efd
권기준 <kkjsw17@naver.com>
2025-02-28 01:31:09 +0900

    feat: swagger 관련 의존성 및 설정 추가

--- clog-api/build.gradle.kts
+++ clog-api/build.gradle.kts
@@ -3,6 +3,7 @@ dependencies {
     implementation(project(":clog-domain"))
 
     implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.8.5")
 }
 
 tasks {
