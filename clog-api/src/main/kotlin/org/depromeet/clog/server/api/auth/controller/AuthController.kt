7c55df5b260e703d420cf362d420e94261de0df7
hyemi <hyemi.choi@tsw.im>
2025-02-28 16:35:07 +0900

    refactor : 테스트 api 삭제

--- clog-api/src/main/kotlin/org/depromeet/clog/server/api/auth/controller/AuthController.kt
+++ clog-api/src/main/kotlin/org/depromeet/clog/server/api/auth/controller/AuthController.kt
@@ -2,21 +2,19 @@ package org.depromeet.clog.server.api.auth.controller
 
 import org.depromeet.clog.server.api.configuration.ApiConstants.API_BASE_PATH_V1
 import org.depromeet.clog.server.domain.auth.application.AuthService
-import org.depromeet.clog.server.domain.auth.application.TokenService
 import org.depromeet.clog.server.domain.auth.application.dto.AppleLoginRequest
 import org.depromeet.clog.server.domain.auth.application.dto.AuthResponseDto
 import org.depromeet.clog.server.domain.auth.application.dto.KakaoLoginRequest
 import org.depromeet.clog.server.domain.common.ApiResponse
-import org.depromeet.clog.server.domain.user.infrastructure.UserRepository
-import org.springframework.http.HttpHeaders
-import org.springframework.web.bind.annotation.*
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping("$API_BASE_PATH_V1/auth")
 class AuthController(
     private val authService: AuthService,
-    private val userRepository: UserRepository,
-    private val tokenService: TokenService,
 ) {
     @PostMapping("/kakao")
     fun kakaoLogin(@RequestBody request: KakaoLoginRequest): ApiResponse<AuthResponseDto> {
@@ -28,14 +26,5 @@ class AuthController(
     fun appleLogin(@RequestBody request: AppleLoginRequest): ApiResponse<AuthResponseDto> {
         val authResponse = authService.appleLoginWithCode(request.code, request.codeVerifier)
         return ApiResponse.success(authResponse)
-    }
-
-    @GetMapping("/test")
-    fun getCurrentUser(@RequestHeader(HttpHeaders.AUTHORIZATION) token: String): ApiResponse<String> {
-        val loginDetails = tokenService.extractLoginDetails(token)
-        val user = userRepository.findByLoginIdAndProvider(loginDetails.loginId, loginDetails.provider)
-            ?: throw RuntimeException("사용자를 찾을 수 없습니다. (loginId: ${loginDetails.loginId})")
-
-        return ApiResponse.success(user.name)
     }
 }
