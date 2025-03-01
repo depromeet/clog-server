2303784ead3f38802e59480d93eab24795dccd70
kkjsw17 <kkjsw17@naver.com>
2025-03-02 01:57:06 +0900

    feat: BaseEntity 추가

--- clog-domain/src/main/kotlin/org/depromeet/clog/server/domain/user/domain/User.kt
+++ clog-domain/src/main/kotlin/org/depromeet/clog/server/domain/user/domain/User.kt
@@ -1,22 +1,9 @@
 package org.depromeet.clog.server.domain.user.domain
 
-import jakarta.persistence.*
-
-@Entity
-class User(
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+data class User(
     val id: Long? = null,
-
-    @Column(nullable = false, unique = true)
     val loginId: String,
-
     val name: String,
-
-    @Enumerated(EnumType.STRING)
-    @Column(nullable = false)
     val provider: Provider,
-
-    @Column(nullable = false)
     var isDeleted: Boolean = false
 )
