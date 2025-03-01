e47af6f69275cd42bd71ee7c8e51e793f3dfe3a3
hym <ghrltjdtprbs@gmail.com>
2025-03-02 00:48:26 +0900

    refactor : 리프레쉬토큰 저장 로직 추가

--- clog-domain/src/main/kotlin/org/depromeet/clog/server/domain/auth/infrastructure/RefreshTokenRepository.kt
+++ clog-domain/src/main/kotlin/org/depromeet/clog/server/domain/auth/infrastructure/RefreshTokenRepository.kt
@@ -1,9 +1,10 @@
 package org.depromeet.clog.server.domain.auth.infrastructure
 
 import org.depromeet.clog.server.domain.auth.domain.RefreshToken
+import org.depromeet.clog.server.domain.user.domain.Provider
 
 interface RefreshTokenRepository {
     fun save(token: RefreshToken): RefreshToken
     fun findById(id: String): RefreshToken?
-    fun delete(token: RefreshToken)
+    fun deleteByLoginIdAndProvider(loginId: String, provider: Provider)
 }
