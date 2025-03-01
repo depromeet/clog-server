23cdd46b81223c980b9ce61624ecf53a150955cf
kkjsw17 <kkjsw17@naver.com>
2025-03-02 01:53:52 +0900

    refactor: Jpa 관련 설정 infrastructure 모듈로 이동 및 api 모듈 내 jpa 디펜던시 제거

--- clog-api/src/main/kotlin/org/depromeet/clog/server/api/ClogApplication.kt
+++ clog-api/src/main/kotlin/org/depromeet/clog/server/api/ClogApplication.kt
@@ -1,13 +1,9 @@
 package org.depromeet.clog.server.api
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
-import org.springframework.boot.autoconfigure.domain.EntityScan
 import org.springframework.boot.runApplication
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories
 
 @SpringBootApplication(scanBasePackages = ["org.depromeet.clog.server"])
-@EntityScan(basePackages = ["org.depromeet.clog.server.domain", "org.depromeet.clog.server.infrastructure"])
-@EnableJpaRepositories(basePackages = ["org.depromeet.clog.server.infrastructure"])
 class ClogApplication
 
 fun main(args: Array<String>) {
