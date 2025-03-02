a3a9904d86a3733fb62878e6a8dbba312e17977e
Kijun Kwon <39583312+kkjsw17@users.noreply.github.com>
2025-03-02 14:37:24 +0900

    refactor: Jpa 관련 설정 infrastructure 모듈로 이동 및 api 모듈 내 jpa 디펜던시 제거 (#45)

--- clog-api/build.gradle.kts
+++ clog-api/build.gradle.kts
@@ -11,9 +11,6 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-oauth2-resource-server")
     implementation("com.auth0:java-jwt:4.4.0")
     implementation("com.auth0:jwks-rsa:0.20.0")
-
-    // @EnableJpaRepositorie 해결하면 삭제해주세요
-    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 }
 
 tasks {
