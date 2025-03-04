a199e37bbcaa6dec4759eeb8b7fbae849d7167dd
hyemi <ghrltjdtprbs@gmail.com>
2025-03-04 16:17:17 +0900

    refactor : ApiResponse에 응답값이 없는 success() 추가

--- clog-domain/src/main/kotlin/org/depromeet/clog/server/domain/common/ApiResponse.kt
+++ clog-domain/src/main/kotlin/org/depromeet/clog/server/domain/common/ApiResponse.kt
@@ -1,11 +1,16 @@
 package org.depromeet.clog.server.domain.common
 
 data class ApiResponse<T>(
-    val data: T? = null,
+    val success: Boolean = true,
+    val data: T? = null
 ) {
     companion object {
         fun <T> from(data: T): ApiResponse<T> {
             return ApiResponse(data = data)
+        }
+
+        fun success(): ApiResponse<Unit> {
+            return ApiResponse(success = true, data = null)
         }
     }
 }
