50c2c8dd3cc2706ba197219d4b8b87781028d779
dae won <eodnjs01477@gmail.com>
2025-03-04 16:36:04 +0900

    refactor: @Repository 어노테이션 추가

--- clog-infrastructure/src/main/kotlin/org/depromeet/clog/server/infrastructure/crag/CragRepositoryAdapter.kt
+++ clog-infrastructure/src/main/kotlin/org/depromeet/clog/server/infrastructure/crag/CragRepositoryAdapter.kt
@@ -2,7 +2,9 @@ package org.depromeet.clog.server.infrastructure.crag
 
 import org.depromeet.clog.server.domain.crag.domain.Crag
 import org.depromeet.clog.server.domain.crag.domain.CragRepository
+import org.springframework.stereotype.Repository
 
+@Repository
 class CragRepositoryAdapter(
     private val cragJpaRepository: CragJpaRepository
 ) : CragRepository {
