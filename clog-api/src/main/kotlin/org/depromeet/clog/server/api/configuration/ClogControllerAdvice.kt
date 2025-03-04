8c09846ac5ff662baa395216faf04249c82983b1
Kijun Kwon <39583312+kkjsw17@users.noreply.github.com>
2025-03-04 11:05:21 +0900

    fix: AuthService 메서드에 트랜잭션이 걸려있지 않은 이슈 수정 (#58)

--- clog-api/src/main/kotlin/org/depromeet/clog/server/api/configuration/ClogControllerAdvice.kt
+++ clog-api/src/main/kotlin/org/depromeet/clog/server/api/configuration/ClogControllerAdvice.kt
@@ -1,5 +1,6 @@
 package org.depromeet.clog.server.api.configuration
 
+import io.github.oshai.kotlinlogging.KotlinLogging
 import org.depromeet.clog.server.domain.common.ClogException
 import org.depromeet.clog.server.domain.common.ErrorResponse
 import org.springframework.http.ResponseEntity
@@ -8,8 +9,12 @@ import org.springframework.web.bind.annotation.RestControllerAdvice
 
 @RestControllerAdvice
 class ClogControllerAdvice {
+
+    private val logger = KotlinLogging.logger { }
+
     @ExceptionHandler(ClogException::class)
     fun handleClogException(e: ClogException): ResponseEntity<ErrorResponse> {
+        logger.error(e) { "ClogException 발생: ${e.message}" }
         return ResponseEntity.status(e.errorCode.httpStatus)
             .body(ErrorResponse.from(e.errorCode, e.detail))
     }
