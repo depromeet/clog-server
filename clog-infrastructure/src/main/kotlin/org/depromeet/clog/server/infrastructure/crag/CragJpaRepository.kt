95ae62c5044f747d41183d786b6c1956807debdb
dae won <eodnjs01477@gmail.com>
2025-03-06 00:05:45 +0900

    feat: Crag 스케줄 추가

--- clog-infrastructure/src/main/kotlin/org/depromeet/clog/server/infrastructure/crag/CragJpaRepository.kt
+++ clog-infrastructure/src/main/kotlin/org/depromeet/clog/server/infrastructure/crag/CragJpaRepository.kt
@@ -2,4 +2,6 @@ package org.depromeet.clog.server.infrastructure.crag
 
 import org.springframework.data.jpa.repository.JpaRepository
 
-interface CragJpaRepository : JpaRepository<CragEntity, Long>
+interface CragJpaRepository : JpaRepository<CragEntity, Long> {
+    fun existsByKakaoPlaceId(kakaoPlaceId: Long): Boolean
+}
