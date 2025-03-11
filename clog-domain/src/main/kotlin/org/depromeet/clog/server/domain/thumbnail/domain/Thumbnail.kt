53e978a125b0cecf3b1ec3e5bde6aad4c6fa9324
hyemi <ghrltjdtprbs@gmail.com>
2025-03-11 17:42:33 +0900

    refactor : 경로이동

--- clog-domain/src/main/kotlin/org/depromeet/clog/server/domain/thumbnail/domain/Thumbnail.kt
+++
@@ -1,6 +0,0 @@
-package org.depromeet.clog.server.domain.thumbnail.domain
-
-data class Thumbnail(
-    val id: Long? = null,
-    val fileUrl: String
-)
--- clog-domain/src/main/kotlin/org/depromeet/clog/server/domain/thumbnail/infrastructure/thumbnail/ThumbnailRepository.kt
+++
@@ -1,7 +0,0 @@
-package org.depromeet.clog.server.domain.thumbnail.infrastructure.thumbnail
-
-import org.depromeet.clog.server.domain.thumbnail.domain.Thumbnail
-
-interface ThumbnailRepository {
-    fun save(thumbnail: Thumbnail): Thumbnail
-}
