0a04716e4ca3bfc18636e96e7e8383c0b6f1c6ac
Kijun Kwon <39583312+kkjsw17@users.noreply.github.com>
2025-03-15 15:22:53 +0900

    [SPS-150] 캘린더 조회 API 내 월 별 요약 정보 한 필드로 정리 (#92)

--- clog-api/src/main/kotlin/org/depromeet/clog/server/api/calendar/application/CalendarResponse.kt
+++ clog-api/src/main/kotlin/org/depromeet/clog/server/api/calendar/application/CalendarResponse.kt
@@ -1,6 +1,7 @@
 package org.depromeet.clog.server.api.calendar.application
 
 import io.swagger.v3.oas.annotations.media.Schema
+import org.depromeet.clog.server.domain.story.Story
 import java.time.LocalDate
 
 @Schema(
@@ -8,28 +9,9 @@ import java.time.LocalDate
 )
 data class CalendarResponse(
     @Schema(
-        description = "해당 월에 클라이밍을 한 날짜 수",
-        example = "5",
+        description = "월 별 정보에 대한 요약입니다.",
     )
-    val numOfClimbDays: Int,
-
-    @Schema(
-        description = "해당 월에 클라이밍을 한 총 시간 (ms)",
-        example = "3600000",
-    )
-    val totalDurationMs: Long,
-
-    @Schema(
-        description = "해당 월에 클라이밍을 한 총 시도 개수",
-        example = "10",
-    )
-    val totalAttemptCount: Int,
-
-    @Schema(description = "시도 성공 횟수", example = "7")
-    val successAttemptCount: Int,
-
-    @Schema(description = "시도 실패 횟수", example = "3")
-    val failAttemptCount: Int,
+    val summary: Summary,
 
     @Schema(
         description = "해당 월에 클라이밍을 한 날짜별 스토리 리스트",
@@ -37,6 +19,52 @@ data class CalendarResponse(
     val days: List<Day>,
 ) {
 
+    @Schema(name = "CalendarResponse.Summary", description = "월 별 정보에 대한 요약입니다.")
+    data class Summary(
+        @Schema(
+            description = "해당 월에 클라이밍을 한 날짜 수",
+            example = "5",
+        )
+        val numOfClimbDays: Int,
+
+        @Schema(
+            description = "해당 월에 클라이밍을 한 총 시간 (ms)",
+            example = "3600000",
+        )
+        val totalDurationMs: Long,
+
+        @Schema(
+            description = "해당 월에 클라이밍을 한 총 시도 개수",
+            example = "10",
+        )
+        val totalAttemptCount: Int,
+
+        @Schema(description = "시도 성공 횟수", example = "7")
+        val successAttemptCount: Int,
+
+        @Schema(description = "시도 실패 횟수", example = "3")
+        val failAttemptCount: Int,
+    ) {
+        companion object {
+            fun from(stories: List<Story>): Summary {
+                val storiesGroupedByDate = stories.groupBy { it.date }
+
+                return Summary(
+                    numOfClimbDays = storiesGroupedByDate.size,
+                    totalDurationMs = stories.sumOf { it.totalDurationMs },
+                    totalAttemptCount = stories.sumOf { it.problems.sumOf { problem -> problem.attempts.size } },
+                    successAttemptCount = stories.sumOf {
+                        it.problems.sumOf { problem -> problem.attempts.count { attempt -> attempt.isSuccess } }
+                    },
+                    failAttemptCount = stories.sumOf {
+                        it.problems.sumOf { problem -> problem.attempts.count { attempt -> !attempt.isFail } }
+                    },
+                )
+            }
+        }
+    }
+
+    @Schema(name = "CalendarResponse.Day", description = "해당 월에 클라이밍을 한 날짜별 기록 리스트")
     data class Day(
         @Schema(
             description = "해당 날짜",
@@ -45,18 +73,19 @@ data class CalendarResponse(
         val date: LocalDate,
 
         @Schema(
-            description = "해당 날짜에 클라이밍을 한 스토리의 썸네일 URL",
+            description = "해당 날짜에 클라이밍을 한 기록의 대표 썸네일 URL",
             example = "https://example.com/thumbnail.jpg",
         )
         val thumbnailUrl: String? = null,
 
         @Schema(
-            description = "해당 날짜에 클라이밍을 한 스토리 리스트",
+            description = "각 날짜에 표시될 기록 목록",
         )
-        val stories: List<Story>,
+        val stories: List<StoryListItem>,
     )
 
-    data class Story(
+    @Schema(name = "CalendarResponse.StoryListItem", description = "각 날짜에 표시될 기록 목록의 아이템")
+    data class StoryListItem(
         @Schema(
             description = "스토리 ID",
             example = "12345",
@@ -81,6 +110,7 @@ data class CalendarResponse(
         val problems: List<Problem>,
     )
 
+    @Schema(name = "CalendarResponse.Problem", description = "해당 기록의 문제 리스트")
     data class Problem(
         @Schema(
             description = "문제의 색상 HEX 코드",
