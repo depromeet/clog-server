7382689d7fb67fc73bb1e70e7077254fc0d320f7
hym <ghrltjdtprbs@gmail.com>
2025-03-15 15:22:49 +0900

    feat : 내가 기록한 암장정보 조회 api 구현

---
+++ clog-api/src/main/kotlin/org/depromeet/clog/server/api/crag/application/GetCrag.kt
@@ -0,0 +1,17 @@
+package org.depromeet.clog.server.api.crag.application
+
+import org.depromeet.clog.server.api.crag.presentation.dto.GetMyCragInfoResponse
+import org.depromeet.clog.server.domain.story.StoryRepository
+import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
+
+@Service
+class GetCrag(
+    private val storyRepository: StoryRepository
+) {
+    @Transactional(readOnly = true)
+    fun getRecordedCrags(userId: Long): List<GetMyCragInfoResponse> {
+        val domainCrags = storyRepository.findDistinctCragsByUserId(userId)
+        return domainCrags.map { GetMyCragInfoResponse(it.id, it.name) }
+    }
+}
---
+++ clog-domain/src/main/kotlin/org/depromeet/clog/server/domain/crag/dto/GetMyCragInfo.kt
@@ -0,0 +1,6 @@
+package org.depromeet.clog.server.domain.crag.dto
+
+data class GetMyCragInfo(
+    val id: Long,
+    val name: String
+)
