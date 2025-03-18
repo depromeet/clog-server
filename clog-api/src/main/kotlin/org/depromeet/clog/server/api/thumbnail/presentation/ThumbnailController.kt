80a9f7c26b002978f0711985aca46db2fb932a81
Kijun Kwon <39583312+kkjsw17@users.noreply.github.com>
2025-03-19 01:17:46 +0900

    [SPS-165] multipart 스웨거에 잘 나오도록 수정 (#102)

--- clog-api/src/main/kotlin/org/depromeet/clog/server/api/thumbnail/presentation/ThumbnailController.kt
+++ clog-api/src/main/kotlin/org/depromeet/clog/server/api/thumbnail/presentation/ThumbnailController.kt
@@ -1,11 +1,13 @@
 package org.depromeet.clog.server.api.thumbnail.presentation
 
 import io.swagger.v3.oas.annotations.Operation
+import io.swagger.v3.oas.annotations.Parameter
 import io.swagger.v3.oas.annotations.tags.Tag
 import org.depromeet.clog.server.api.configuration.ApiConstants.API_BASE_PATH_V1
 import org.depromeet.clog.server.domain.common.ClogApiResponse
 import org.depromeet.clog.server.domain.thumbnail.application.ThumbnailService
 import org.depromeet.clog.server.domain.thumbnail.application.ThumbnailUploadResponse
+import org.springframework.http.MediaType
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestPart
@@ -20,8 +22,9 @@ class ThumbnailController(
 ) {
 
     @Operation(summary = "썸네일 업로드")
-    @PostMapping("/upload")
+    @PostMapping("/upload", consumes = [MediaType.MULTIPART_FORM_DATA_VALUE])
     fun uploadImage(
+        @Parameter(description = "썸네일 이미지 파일을 key=`file`로 보내주세요.")
         @RequestPart("file")
         file: MultipartFile
     ): ClogApiResponse<ThumbnailUploadResponse> {
