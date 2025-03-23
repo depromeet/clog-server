edc6a17dcd08bfc741aa994e46963b1f752618bf
kkjsw17 <kkjsw17@naver.com>
2025-03-23 12:57:55 +0900

    [SPS-180] liveness/readiness health check 추가

--- clog-api/src/main/kotlin/org/depromeet/clog/server/api/ClogApplication.kt
+++ clog-api/src/main/kotlin/org/depromeet/clog/server/api/ClogApplication.kt
@@ -3,11 +3,13 @@ package org.depromeet.clog.server.api
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.scheduling.annotation.EnableScheduling
+import java.util.TimeZone
 
 @SpringBootApplication(scanBasePackages = ["org.depromeet.clog.server"])
 @EnableScheduling
 class ClogApplication
 
 fun main(args: Array<String>) {
+    TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"))
     runApplication<ClogApplication>(*args)
 }
