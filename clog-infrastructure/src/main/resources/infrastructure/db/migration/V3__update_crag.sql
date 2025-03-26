112a974723f675c75c8a32e775483d09d0b7b3ce
dae won <eodnjs01477@gmail.com>
2025-03-27 02:32:00 +0900

    refactor: Rename columns to snake_case in Flyway script.

--- clog-infrastructure/src/main/resources/infrastructure/db/migration/V3__update_crag.sql
+++ clog-infrastructure/src/main/resources/infrastructure/db/migration/V3__update_crag.sql
@@ -1,4 +1,4 @@
 -- 1. lotNumberAddress 컬럼 추가
 ALTER TABLE crag
-    ADD COLUMN lotNumberAddress VARCHAR(255) NOT NULL COMMENT '지번 주소';
+    ADD COLUMN lot_number_address VARCHAR(255) NOT NULL COMMENT '지번 주소';
 
