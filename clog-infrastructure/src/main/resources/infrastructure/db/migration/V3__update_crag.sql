8345b09d7695fb0cece66d21f7829c3451ec8a5d
dae won <eodnjs01477@gmail.com>
2025-03-27 09:42:05 +0900

    refactor: flyway if not exists 추가

--- clog-infrastructure/src/main/resources/infrastructure/db/migration/V3__update_crag.sql
+++ clog-infrastructure/src/main/resources/infrastructure/db/migration/V3__update_crag.sql
@@ -1,4 +1,4 @@
 -- 1. lotNumberAddress 컬럼 추가
 ALTER TABLE crag
-    ADD COLUMN lot_number_address VARCHAR(255) NOT NULL COMMENT '지번 주소';
+    ADD COLUMN IF NOT EXISTS lot_number_address VARCHAR(255) NOT NULL COMMENT '지번 주소';
 
