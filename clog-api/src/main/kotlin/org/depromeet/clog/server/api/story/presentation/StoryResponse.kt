a6f75f298b2bad14b8489901e7133767d81f7e18
Kijun Kwon <39583312+kkjsw17@users.noreply.github.com>
2025-03-29 20:27:38 +0900

    feat: 기록 상세 조회 API -> 총 운동 시간 추가 (#136)

--- clog-api/src/main/kotlin/org/depromeet/clog/server/api/story/presentation/StoryResponse.kt
+++ clog-api/src/main/kotlin/org/depromeet/clog/server/api/story/presentation/StoryResponse.kt
@@ -4,10 +4,16 @@ import io.swagger.v3.oas.annotations.media.Schema
 import org.depromeet.clog.server.api.problem.presentation.ProblemResponse
 import org.depromeet.clog.server.domain.story.StoryQuery
 
-@Schema(description = "스토리 응답 DTO")
+@Schema(description = "기록 응답 DTO")
 data class StoryResponse(
-    @Schema(description = "스토리 ID", example = "1")
+    @Schema(description = "기록 ID", example = "1")
     val id: Long,
+
+    @Schema(
+        description = "해당 기록의 총 클라이밍 시간 (ms)",
+        example = "3600000",
+    )
+    val totalDurationMs: Long,
 
     @Schema(description = "문제 리스트")
     val problems: List<ProblemResponse>,
@@ -16,6 +22,7 @@ data class StoryResponse(
         fun from(storyQuery: StoryQuery): StoryResponse {
             return StoryResponse(
                 id = storyQuery.id,
+                totalDurationMs = storyQuery.totalDurationMs,
                 problems = storyQuery.problems.map { ProblemResponse.from(it) },
             )
         }
