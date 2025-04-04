559e0e34031102f82f3803f194199406cfd385f3
권기준 <kkjsw17@naver.com>
2025-04-04 14:40:58 +0900

    [SPS-279] 썸네일 이미지 1:1 비율로 자르는 기능 임시 추가

--- clog-infrastructure/src/main/kotlin/org/depromeet/clog/server/infrastructure/thumbnail/NcpObjectStorageClient.kt
+++ clog-infrastructure/src/main/kotlin/org/depromeet/clog/server/infrastructure/thumbnail/NcpObjectStorageClient.kt
@@ -10,6 +10,7 @@ import com.amazonaws.services.s3.model.ObjectMetadata
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Component
 import org.springframework.web.multipart.MultipartFile
+import java.io.ByteArrayInputStream
 import java.util.*
 
 @Component
@@ -18,7 +19,8 @@ class NcpObjectStorageClient(
     @Value("\${ncp.storage.region}") private val region: String,
     @Value("\${ncp.storage.access-key}") private val accessKey: String,
     @Value("\${ncp.storage.secret-key}") private val secretKey: String,
-    @Value("\${ncp.storage.end-point}") private val endpoint: String
+    @Value("\${ncp.storage.end-point}") private val endpoint: String,
+    @Value("\${thumbnail.image.crop.enable}") private val isCropEnable: Boolean,
 ) {
 
     private val s3Client: AmazonS3 = run {
@@ -32,13 +34,24 @@ class NcpObjectStorageClient(
 
     fun uploadFile(file: MultipartFile): String {
         val fileName = generateUniqueFileName(file.originalFilename)
-        val metadata = ObjectMetadata().apply {
-            contentLength = file.size
-            contentType = file.contentType
-        }
-        s3Client.putObject(bucketName, fileName, file.inputStream, metadata)
-        s3Client.setObjectAcl(bucketName, fileName, CannedAccessControlList.PublicRead) // ✅ 추가됨
 
+        if (isCropEnable && file.contentType?.equals("image/png", ignoreCase = true) == true) {
+            val croppedBytes = ImageCropper.cropPngImage(file)
+            val croppedInputStream = ByteArrayInputStream(croppedBytes)
+            val metadata = ObjectMetadata().apply {
+                contentLength = croppedBytes.size.toLong()
+                contentType = "image/png"
+            }
+            s3Client.putObject(bucketName, fileName, croppedInputStream, metadata)
+            s3Client.setObjectAcl(bucketName, fileName, CannedAccessControlList.PublicRead)
+        } else {
+            val metadata = ObjectMetadata().apply {
+                contentLength = file.size
+                contentType = file.contentType
+            }
+            s3Client.putObject(bucketName, fileName, file.inputStream, metadata)
+            s3Client.setObjectAcl(bucketName, fileName, CannedAccessControlList.PublicRead)
+        }
         return s3Client.getUrl(bucketName, fileName).toString()
     }
 
