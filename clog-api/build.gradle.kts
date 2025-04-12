5f32c4e7c004e4872d3fcd552408ed5bdc83949a
dae won <99483390+big-cir@users.noreply.github.com>
2025-04-12 23:39:53 +0900

    logback 추가 (#26)

--- clog-api/build.gradle.kts
+++ clog-api/build.gradle.kts
@@ -14,6 +14,9 @@ dependencies {
     implementation("com.auth0:java-jwt:4.4.0")
     implementation("com.auth0:jwks-rsa:0.20.0")
     implementation("io.jsonwebtoken:jjwt-api:0.11.5")
+
+    implementation("net.logstash.logback:logstash-logback-encoder:8.1")
+
     runtimeOnly("io.jsonwebtoken:jjwt-impl:0.11.5")
     runtimeOnly("io.jsonwebtoken:jjwt-jackson:0.11.5")
 }
