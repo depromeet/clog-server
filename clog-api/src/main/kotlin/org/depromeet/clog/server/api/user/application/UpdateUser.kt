1329c2c087992be3e7c6330bdcf4fc8cec423fe1
Kijun Kwon <39583312+kkjsw17@users.noreply.github.com>
2025-06-02 03:00:53 +0900

    [PLT-9] validation 수정 (#174)

--- clog-api/src/main/kotlin/org/depromeet/clog/server/api/user/application/UpdateUser.kt
+++ clog-api/src/main/kotlin/org/depromeet/clog/server/api/user/application/UpdateUser.kt
@@ -3,7 +3,6 @@ package org.depromeet.clog.server.api.user.application
 import io.swagger.v3.oas.annotations.media.Schema
 import jakarta.validation.constraints.Max
 import jakarta.validation.constraints.Min
-import jakarta.validation.constraints.NotBlank
 import jakarta.validation.constraints.Pattern
 import org.depromeet.clog.server.domain.user.domain.UserRepository
 import org.depromeet.clog.server.domain.user.domain.exception.UserNotFoundException
@@ -22,8 +21,11 @@ class UpdateUser(
         val user = userRepository.findByIdAndIsDeletedFalse(userId)
             ?: throw UserNotFoundException()
 
+        val name = command.name?.trim()
+            ?.takeIf { it.isNotBlank() }
+
         user.copy(
-            name = command.name ?: user.name,
+            name = name ?: user.name,
             height = command.height ?: user.height,
             armSpan = command.armSpan ?: user.armSpan,
             instagramUrl = command.instagramUrl ?: user.instagramUrl,
@@ -35,25 +37,25 @@ class UpdateUser(
     }
 
     data class Command(
-        @field:NotBlank
         @Length(min = 1, max = 10)
-        @Schema(title = "이름")
+        @Schema(title = "이름", example = "권기준")
         val name: String? = null,
 
         @field:Min(1)
         @field:Max(999)
-        @Schema(title = "키", description = "1 ~ 999 사이의 값")
+        @Schema(title = "키", description = "1 ~ 999 사이의 값", example = "180")
         val height: Int? = null,
 
         @field:Min(1)
         @field:Max(999)
-        @Schema(title = "팔 길이", description = "1 ~ 999 사이의 값")
+        @Schema(title = "팔 길이", description = "1 ~ 999 사이의 값", example = "190")
         val armSpan: Int? = null,
 
         @field:Pattern(
             regexp = "^https://(www\\.)?instagram\\.com/[A-Za-z0-9._]{1,30}/?$",
             message = "유효한 인스타그램 프로필 URL을 입력하세요.",
         )
+        @Schema(title = "인스타그램 URL", example = "https://www.instagram.com/kkjsw17/")
         val instagramUrl: String? = null,
     )
 
